62350949a10262a8126c93f9c29666805b3bf44f
Zandalor-DG <paliy.zdg@gmail.com>
2021-03-03 23:48:09 +0300

    Create project

---
+++ BookStore.DB/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentNHibernate.Mapping;
+
+namespace BookStore.DB.Models
+{
+    public class User
+    {
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string Email { get; set; }
+
+        public class UserMap : ClassMap<User>
+        {
+            public UserMap()
+            {
+                Id(x => x.Id);
+                Map(x => x.FirstName);
+                Map(x => x.Email);
+            } 
+        }
+    }
+}
